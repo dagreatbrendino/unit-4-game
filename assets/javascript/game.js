b77f2e64bc5b2ea4cac82847cb96e55ee124eee0
Brendan K <brendanmkelly94@gmail.com>
2018-12-15 01:34:05 -0600

    player is now able to intialize their character, and then pick the first enemy to fight fromthe remaining characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,11 @@
 console.log("i am ready");
 var allFighters = [{ name: "Mace Windu", hp: 150, atk: 8, counter: 25, alive: true, picked: false },
-{ name: "Kylo Ren", hp: 100, atk: 6, counter: 10, alive: true, picked: false }];
+{ name: "Kylo Ren", hp: 100, atk: 6, counter: 10, alive: true, picked: false },
+{ name: "Boba Fett", hp: 140, atk: 8, counter: 5, alive: true, picked: false}];
+//Fighters left is intialized to be the same as all fighters. Items will be removed from this index as they are picked/defeated
+// var fightersLeft =  [{ name: "Mace Windu", hp: 150, atk: 8, counter: 25, alive: true, picked: false },
+// { name: "Kylo Ren", hp: 100, atk: 6, counter: 10, alive: true, picked: false }];
+
 var fighterSelected = false;
 
 var playerFighter = {
@@ -62,28 +67,43 @@ var enemyFighter = {
     }
 
 }
-var populateFighters = function(){
-    $.each(allFighters, function(i, fighter){
-        $("#char-selection").append('<div class="col-2 character" value='+ i + '><p>' + allFighters[i].name + "</p></div>");
+//This function will add a character to the dom for every charater in the array passed to it
+var populateFighters = function(arr){
+    $.each(arr, function(i, fighter){
+        $("#char-selection").append('<div class="col-2 character" value='+ i + '><p>' + arr[i].name + "</p></div>");
 
     })
 }
 
-$(document).ready(function(){
-populateFighters();
-    
-$(".character").on("click", function(){
-    debugger
-    console.log("my value is" + $(this).attr("value"));
-    if (!fighterSelected){
-        debugger
-        var index= parseInt($(this).attr("value"));
-        playerFighter.setindexofFighter(index);
-        playerFighter.setplayerAtk();
-        playerFighter.setplayerHP();
-        $("#player > div > img").attr("src", ("assets/images/") + allFighters[index].name.split(' ').join('')+ ".jpg");
-        fighterSelected = true;
-    }
-});
+$(document).ready(function () {
+    populateFighters(allFighters);
+    //When one of the character icons is clicked this function will be called
+    $(".character").on("click", function () {
+        //If the player has not yet chosen their character, the player character object will have its values reassigned to the same values as the character the player clicked on
+        //after that the player will no longer be able to select a fighter
+        if (!fighterSelected) {
+            debugger
+            var index = parseInt($(this).attr("value"));
+            playerFighter.setindexofFighter(index);
+            playerFighter.setplayerAtk();
+            playerFighter.setplayerHP();
+            $("#player > div > img").attr("src", ("assets/images/") + allFighters[index].name.split(' ').join('') + ".jpg");
+            fighterSelected = true;
+            // fightersLeft.splice(index,1);
+            $(this).remove();
+        }
+        //If the player has already chosen their character and they click on a different character, if they are not currently fighting an enemy, the enemy fighter object
+        //will have it's values reassigned to the values of the character the player clicked on. The player will not be able to select another enemy until they have defeated
+        //the current enemy
+        else if (!enemySelected){
+            var index = parseInt($(this).attr("value"));
+            enemyFighter.setindexofEnemy(index);
+            enemyFighter.setenemyAtk();
+            enemyFighter.setenemyHP();
+            $("#defender > div > img").attr("src", ("assets/images/") + allFighters[index].name.split(' ').join('') + ".jpg");
+            enemySelected = true;
+            $(this).remove();
+        }
+    });
 
 });
